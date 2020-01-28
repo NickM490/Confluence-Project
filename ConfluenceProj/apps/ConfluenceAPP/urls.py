025452233379c62ea8f2a07503792b4ba7a8443c
NickM490 <54374349+NickM490@users.noreply.github.com>
2020-01-28 11:57:21 -0800

    Initial Commit

---
+++ ConfluenceProj/apps/ConfluenceAPP/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^landing', views.landing),
+    url(r'^go_process', views.go_process),
+    url(r'^batman', views.batman),
+]
